7399f75f9209fda9ca88387c8877e543bbe65579
@dewani12 <gauravdewani2@gmail.com>
2024-12-06 14:11:00 +0530

    db connected

--- Backend/app.js
+++ Backend/app.js
@@ -4,9 +4,19 @@ const express = require('express')
 const app = express()
 const http = require('http')
 
+const mongoose = require('mongoose')
+
 const server = http.createServer(app)
 
 const port = process.env.PORT || 3000
+
+// connect to mongoDB
+try {
+    mongoose.connect(process.env.DB_CONNECT)
+    console.log('connected to db')
+} catch (err) {
+    console.log(err);
+}
 
 app.get('/',(req,res)=>{
     res.send('Hello World!')
