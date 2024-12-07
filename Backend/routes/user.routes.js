1208d09abcef88479f7fcd29e03ce1c1e6af229e
@dewani12 <gauravdewani2@gmail.com>
2024-12-07 14:13:03 +0530

    /users/login added

--- Backend/routes/user.routes.js
+++ Backend/routes/user.routes.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const router = express.Router()
-const {registerUser} = require('../controllers/user.controller')
+const {registerUser,loginUser} = require('../controllers/user.controller')
 const {body} = require('express-validator')
 
 router.post('/register',[
@@ -8,5 +8,10 @@ router.post('/register',[
     body('fullname.firstname').isLength({min: 3}).withMessage('First Name must be atleast 3 characters long'),
     body('password').isLength({min: 3}).withMessage('Password must be atleast 6 characters long')
 ],registerUser)
+
+router.post('/login',[
+    body('email').isEmail().withMessage('Invalid Email'),
+    body('password').isLength({min: 3}).withMessage('Password must be atleast 6 characters long')
+],loginUser)
 
 module.exports = router
