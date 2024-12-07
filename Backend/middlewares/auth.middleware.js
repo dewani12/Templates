cb664d22ad22135ca0a771a9a92a03e41363f675
@dewani12 <gauravdewani2@gmail.com>
2024-12-08 01:11:29 +0530

    user authentication

--- Backend/middlewares/auth.middleware.js
+++ Backend/middlewares/auth.middleware.js
@@ -1,12 +1,13 @@
 const userModel = require('../models/user.model')
-const bcrypt= require('bcrypt')
 const jwt = require('jsonwebtoken')
 
 module.exports.authUser = async(req,res,next)=>{
-    const token = req.cookies.token || req.headers.authorization.split(' ')[1]
+    const token = req.cookies.token || req.headers.authorization?.split(' ')[1]
+
     if(!token){
         return res.status(401).json({mssg: 'Unauthorized'})
     }
+
     try {
         const decodedId = jwt.verify(token, process.env.JWT_SECRET)
         const user = await userModel.findById(decodedId._id)
